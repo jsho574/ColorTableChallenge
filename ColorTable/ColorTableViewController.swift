7fbbe790d3a157f96a111feb08cc2561163ea518
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-03-08 09:45:20 -0600

    edited the viewController

--- ColorTable/ColorTableViewController.swift
+++ ColorTable/ColorTableViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ColorTableViewController: UIViewController {
+class ColorTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,21 @@ class ColorTableViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
